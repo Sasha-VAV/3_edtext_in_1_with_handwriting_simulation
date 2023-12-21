be24f47af6ea7d72ac73f4b477e7564adbff3148
Sashavav <vav110011@gmail.com>
2023-12-21 15:18:55 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "3 edtext in 1 with hand writing"
+include(":app")
+ 
